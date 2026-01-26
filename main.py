83cee4e717cc2f20509d29a39f64a981bf3f7d3c
Sarrveshwer <sarrveshwerv@gmail.com>
2026-01-26 18:23:36 +0530

    only readme file left may add additional features later

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import RandomizedSearchCV
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve, average_precision_score
+from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve, average_precision_score,confusion_matrix , ConfusionMatrixDisplay
 from sklearn.pipeline import Pipeline
 import pandas as pd
 import numpy as np
@@ -8,7 +9,12 @@ import matplotlib.pyplot as plt
 import seaborn as sns 
 import os
 import auto_logger
+import gc
 
+sns.set_theme(
+    context="talk",   # larger fonts without shouting
+    palette="crest_r"
+)
 
 if __name__ == '__main__':
     auto_logger.setup_logging('main.py')
@@ -18,7 +24,7 @@ try:
 except:
     pass
 
-class LogisticCassifier:
+class LogisticClassifier:
     def __init__(self):
         self.y = None
         self.x = None
@@ -47,62 +53,153 @@ class LogisticCassifier:
         self.X_test  = self.X.iloc[idx:]
         self.y_test  = self.y.iloc[idx:]
 
-        print(*self.X.columns, sep='\n')
+        
     def train_run(self):
-        pipe = Pipeline(
-            steps=[
-                    ("scaler", StandardScaler()),
-                    ("clf", LogisticRegression(
-                        class_weight="balanced",
-                        solver="liblinear",
-                        max_iter=1000
-                        ))
-                    ]
-                )
-        pipe.fit(self.X_train,self.y_train)
-        self.y_pred = pipe.predict(self.X_test)
-        self.y_prob = pipe.predict_proba(self.X_test)[:, 1]
-        roc = roc_auc_score(self.y_test, self.y_prob)
-        print("ROC-AUC Score:", roc)
-        self.plot(self.y,self.y_pred,roc)
-    def plot(self,y,y_pred,roc):
-        #ROC-AUC Plot
+        pipe = Pipeline([
+            ("scaler", StandardScaler()),
+            ("clf", LogisticRegression(
+                solver="lbfgs",
+                class_weight="balanced",
+                max_iter=3000
+            ))
+        ])
+
+        param_dist = {
+             "clf__C": np.logspace(-3, 2, 50)
+        }
+        search = RandomizedSearchCV(
+            pipe,
+            param_distributions=param_dist,
+            n_iter=50,
+            scoring="roc_auc",
+            cv=5,
+            random_state=42,
+            n_jobs=-1,
+            verbose = 1,
+            error_score='raise'
+        )
+        search.fit(self.X_train,self.y_train)
+        self.y_prob = search.predict_proba(self.X_test)[:, 1]
+        self.roc = roc_auc_score(self.y_test, self.y_prob)
+        self.plot(self.roc)
+    def plot(self,roc):
+        
+        palette = sns.color_palette("crest", 10)
+
+        # Dashboard layout
+        layout = [["confusion", "roc"],
+                ["prc", "kde"]]
+
+        fig, axes = plt.subplot_mosaic(layout, figsize=(12, 10))
+
+        #ROC AUC plot
         fpr, tpr, _ = roc_curve(self.y_test, self.y_prob)
 
-        plt.figure(figsize=(5,5))
-        plt.plot(fpr, tpr, label=f"ROC (AUC={roc:.3f})")
-        plt.plot([0,1],[0,1],'--',color='gray')
-        plt.xlabel("False Positive Rate")
-        plt.ylabel("True Positive Rate")
-        plt.legend()
-        plt.title("ROC Curve – Time-Aware Validation")
-        plt.savefig(f'images/ROC-AUC_{self.filename}.png')
-        plt.show()
-        
-        #Risk Score Distribution
-        plt.figure(figsize=(6,4))
-        sns.kdeplot(self.y_prob[self.y_test == 0], label="No Fault", fill=True)
-        sns.kdeplot(self.y_prob[self.y_test == 1], label="Fault", fill=True)
-        plt.xlabel("Predicted Risk Score")
-        plt.title("Risk Score Distribution by Class")
-        plt.legend()
-        plt.savefig(f'images/Risk-Score-Distribution_{self.filename}.png')
-        plt.show()
-        
-        #Precision Recall curve
-        precision, recall, _ = precision_recall_curve(self.y_test, self.y_prob)
+        axes["roc"].plot(fpr, tpr, lw=2.5, label=f"AUC = {roc:.3f}")
+        axes["roc"].plot([0,1], [0,1], ls="--", color="gray", alpha=0.6)
+        axes["roc"].fill_between(fpr, tpr, alpha=0.15)
+
+        axes["roc"].set_xlabel("False Positive Rate")
+        axes["roc"].set_ylabel("True Positive Rate")
+        axes["roc"].set_title("ROC Curve")
+        axes["roc"].legend()
+
+        #KDE plot for Risk Score
+        sns.kdeplot(
+            self.y_prob[self.y_test == 0],
+            fill=True,
+            color=palette[1],
+            alpha=0.6,
+            linewidth=2,
+            label="No Failure",
+            ax=axes["kde"]
+        )
+
+        sns.kdeplot(
+            self.y_prob[self.y_test == 1],
+            fill=True,
+            color=palette[7],
+            alpha=0.6,
+            linewidth=2,
+            label="Failure",
+            ax=axes["kde"]
+        )
+
+        axes["kde"].set_xlabel("Predicted Risk Score")
+        axes["kde"].set_title("Risk Score Distribution")
+        axes["kde"].legend()
+
+        # Precision–Recall Curve
+        precision, recall, thresholds = precision_recall_curve(self.y_test, self.y_prob)
         ap = average_precision_score(self.y_test, self.y_prob)
 
-        plt.figure(figsize=(5,4))
-        plt.plot(recall, precision, label=f"AP={ap:.3f}")
-        plt.xlabel("Recall")
-        plt.ylabel("Precision")
-        plt.title("Precision–Recall Curve")
-        plt.legend()
-        plt.savefig(f'images/Precision-Recall-Curve_{self.filename}.png')
+        axes["prc"].plot(recall, precision, lw=2.5, label=f"AP = {ap:.3f}")
+        axes["prc"].set_xlabel("Recall")
+        axes["prc"].set_ylabel("Precision")
+        axes["prc"].set_title("Precision–Recall Curve")
+        axes["prc"].legend()
+
+        #Confusion Matrix
+        target_recall = 0.80
+        idx = np.where(recall >= target_recall)[0][-1]
+        threshold = thresholds[idx]
+
+        y_pred_thr = (self.y_prob >= threshold).astype(int)
+
+        cm = confusion_matrix(self.y_test, y_pred_thr)
+        tn, fp, fn, tp = cm.ravel()
+
+        cm_percent = cm / cm.sum() * 100
+        #labelling each cell of confusion matrix
+        labels = np.array([
+            [f"TN:{tn}\n({cm_percent[0,0]:.1f}%)", f"FP:{fp}\n({cm_percent[0,1]:.1f}%)"],
+            [f"FN:{fn}\n({cm_percent[1,0]:.1f}%)", f"TP:{tp}\n({cm_percent[1,1]:.1f}%)"]
+        ])
+
+        sns.heatmap(
+            cm / cm.max(),   # normalize colors
+            annot=labels,
+            fmt="",
+            cmap="crest",
+            linewidths=0.5,
+            linecolor="white",
+            xticklabels=["No Failure", "Failure"],
+            yticklabels=["No Failure", "Failure"],
+            alpha = 0.6,
+            cbar=False,
+            ax=axes["confusion"],
+        )
+
+        axes["confusion"].set_title(f"Confusion Matrix (threshold={threshold:.2f})")
+        axes["confusion"].set_xlabel("Predicted")
+        axes["confusion"].set_ylabel("Actual")
+        axes["confusion"].grid(False)
+
+        plt.tight_layout()
+        fig.savefig(f"images/Dashboard_{self.filename}.png", dpi=150)
         plt.show()
+        self.threshold = threshold
+        self.y_pred_thr = y_pred_thr
+        self.evaluate()
+    def evaluate(self):
+        tn, fp, fn, tp = confusion_matrix(self.y_test, self.y_pred_thr).ravel()
+        print("========== Model Features =========")
+        print(*self.X.columns, sep='\n')
+        print("\n=== Model Evaluation (Test Set) ===")
+        print(f"ROC-AUC score :{self.roc:.3f}")
+        print(f"Threshold used : {self.threshold:.3f}")
+        print(f"TP: {tp} | FP: {fp}")
+        print(f"FN: {fn} | TN: {tn}")
+
+        print("\nRates:")
+        print(f"Recall (TPR) : {tp / (tp + fn):.3f}")
+        print(f"Precision    : {tp / (tp + fp):.3f}")
+        print(f"False PosRate: {fp / (fp + tn):.3f}")
+        
+        print("====================================")
+        gc.collect()
 if __name__ == '__main__':
-    model = LogisticCassifier()
+    model = LogisticClassifier()
     model.dataset('ai4i2020.csv','Machine failure',['TWF','HDF','PWF','OSF','RNF','UDI','Product ID','Type'])
     model.train_run()
     
